ca3a3d68ba055ee2e02fd49eaeeb1ec5e4e332ed
Justin Jerez <jrz.09@hotmail.com>
2020-07-24 19:32:45 -0400

    Welcome message

---
+++ functions/abstracts.py
@@ -0,0 +1,20 @@
+""" IMPORTS """
+import time
+from os import name, system
+
+# Clean Consonsole
+def clear_console():
+    if name == 'nt':
+        system('cls')
+    else:
+        system('clear')
+
+# Welcome presentation
+def welcome():
+    print('UNO GAME')
+    time.sleep(1)
+    print('By Justin Jerez')
+    time.sleep(1)
+    print('A Cincinnatus project')
+    time.sleep(1)
+    input('\n Press any key to start the game...')
---
+++ main.py
@@ -0,0 +1,6 @@
+from functions.abstracts import *
+
+
+# Welcome presentation
+clear_console()
+welcome()
